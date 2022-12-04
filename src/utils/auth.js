fc22d7cf96c4d958d00519f35f64843efdf2f3ba
Alex Gus <radiogusarov@gmail.com>
2022-12-04 08:18:10 +0300

    Fix checkResponse in auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,21 @@
 const BASE_URL = 'https://auth.nomoreparties.co';
 
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
 export const register = (email, password) => {
-  console.log(email, password, 'in register')
+  console.log(email, password, 'in auth.register')
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ email, password })
+    body: JSON.stringify({ password, email })
   })
     .then(checkResponse)
 };
@@ -38,6 +45,3 @@ export const checkToken = (token) => {
     .then(checkResponse)
 }
 
-const checkResponse = (res) => {
-  res.ok ? res.json() : Promise.reject(`Ошибка: ${res.statusText}`);
-}
